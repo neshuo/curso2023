af9e856e0d2d181bf477b344d9eb3036ab2b80fd
Neshuo <nestor_agut@hotmail.com>
2023-04-24 16:39:49 +0000

    Terminado 2 dia

---
+++ helpdesk_NestorAgut/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "helpdesk Nestor Agut",
+    "summary": "Gestion incidencias de nestor",
+    "version": "16.0.1.0.0",
+    "category": "Helpdesk",
+    "website": "https://github.com/neshuo",
+    "author": "Datanalisis, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/helpdesk_security.xml",
+        "views/helpdesk_Nestor.xml",
+        "security/ir.model.access.csv"
+    ],
+}
