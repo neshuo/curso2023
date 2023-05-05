697f8c55b06fd56bc408804b27ac1439a9b04dd2
Neshuo <nestor_agut@hotmail.com>
2023-05-05 14:26:53 +0000

    tercera semana compeltada

--- helpdesk_NestorAgut/__manifest__.py
+++ helpdesk_NestorAgut/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "helpdesk Nestor Agut",
+    "name": "helpdesk_NestorAgut",
     "summary": "Gestion incidencias de nestor",
     "version": "16.0.1.0.0",
     "category": "Helpdesk",
